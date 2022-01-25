9a5c844be85503ea0a276d08c8aa5d45f7f4853e
fabien-ors <fabien.ors@mines-paristech.de>
2022-01-25 14:48:21 +0100

    Add VectorInt and String typedefs

---
+++ include/fibo_define.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <vector>
+#include <string>
+
+//#include "myfibo_export.hpp"
+
+#define DEFAULT_TITLE "Fibonacci"
+
+typedef std::string String;
+
+//template class MYFIBO_EXPORT std::vector<int>;
+typedef std::vector<int> VectorInt;

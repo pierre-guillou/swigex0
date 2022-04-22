f7eb54469bb18f4a0a0a90bd63343c3d49e8c452
fabien-ors <fabien.ors@mines-paristech.fr>
2022-04-22 22:42:15 +0200

    Now this is good, I guess git status!

--- tests/r/main.r
+++ tests/r/main.r
@@ -1,5 +1,5 @@
-source("../../r/rfibo_loader.R")
-load_myfibo("../../build/r/Release/myfibo")
+source(file.path(Sys.getenv("MYFIBO_SOURCE_DIR"),"r/rfibo_loader.R"))
+load_myfibo(file.path(Sys.getenv("MYFIBO_SOURCE_DIR"),"build/r/Release/myfibo"))
 
 vec = fib(40)
 print(vec)
@@ -7,5 +7,4 @@ f1 = Fibo(50, "Test 50")
 f1$display()
 f2 = Fibo(100, "Test 100")
 f2$display()
-
 
